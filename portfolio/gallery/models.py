056c78a5bb8b24381c6b73441c18b45fbde66a5f
chenBW <xero2022@163.com>
2020-05-26 15:00:27 +0800

    设置图片

--- portfolio/gallery/models.py
+++ portfolio/gallery/models.py
@@ -3,7 +3,14 @@ from django.db import models
 # Create your models here.
 
 class Gallery(models.Model):
-    desription = models.CharField(max_length=100)
+    desription = models.CharField(default='作品的描述',max_length=100)
+    pic = models.ImageField(default='default.png',upload_to='images/')
+    title = models.CharField(default='这里写简介',max_length=100)
+
+    def __str__(self):
+        return self.title
+
+
 
 
 
--- portfolio/portfolio/views.py
+++ portfolio/portfolio/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from gallery.models import Gallery
 
 def home(request):
     return render(request,'home.html')
