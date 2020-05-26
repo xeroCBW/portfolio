2056dd091b3468cf5b1bafefa4a8f7a30bdc4c4a
chenBW <xero2022@163.com>
2020-05-26 17:07:24 +0800

    初步完成blog

---
+++ portfolio/blog/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Blog
+# Register your models here.
+
+# 注册model
+admin.site.register(Blog)
--- portfolio/portfolio/urls.py
+++ portfolio/portfolio/urls.py
@@ -16,10 +16,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from . import views
+import gallery.views
+import blog.views
 from django.conf.urls.static import static
 from django.conf import settings
+from django.conf.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home),
+    path('blog/',include('blog.urls')),
+
 ] + static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
